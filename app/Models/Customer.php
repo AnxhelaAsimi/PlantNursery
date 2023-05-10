94fc16f00b816b8236463710d4f2e629a544ade6
AnxhelaAsimi <87004166+AnxhelaAsimi@users.noreply.github.com>
2023-05-10 09:10:11 +0200

    Manage Customers

---
+++ app/Models/Customer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Image;
+
+class Customer extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+
+    //Stores a customer into the database
+    public function CustomerStore(Request $request){
+
+        $image = $request->file('customer_image');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension(); // 343434.png
+        Image::make($image)->resize(200,200)->save('upload/customer/'.$name_gen);
+        $save_url = 'upload/customer/'.$name_gen;
+
+        Customer::insert([
+            'name' => $request->name,
+            'mobile_no' => $request->mobile_no,
+            'email' => $request->email,
+            'address' => $request->address,
+            'customer_image' => $save_url ,
+            'created_by' => Auth::user()->id,
+            'created_at' => Carbon::now(),
+
+        ]);
+
+         $notification = array(
+            'message' => 'Customer Inserted Successfully', 
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('customer.all')->with($notification);
+
+    } // End Method
+}
