299aa254b350ba89c5d615c9ab5f9540d4534594
AnxhelaAsimi <87004166+AnxhelaAsimi@users.noreply.github.com>
2023-05-16 15:13:53 +0200

    Edit Customer Invoice

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,9 @@ Route::middleware('auth')->group(function () {
         Route::get('/customer/delete/{id}', 'CustomerDelete')->name('customer.delete');
         Route::get('/credit/customer', 'CreditCustomer')->name('credit.customer');
         Route::get('/credit/customer/print/pdf', 'CreditCustomerPrintPdf')->name('credit.customer.print.pdf');
+        Route::get('/customer/edit/invoice/{invoice_id}', 'CustomerEditInvoice')->name('customer.edit.invoice');
+        Route::post('/customer/update/invoice/{invoice_id}', 'CustomerUpdateInvoice')->name('customer.update.invoice');
+
     });
 
     //Units Module Routes
